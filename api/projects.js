3c63a792316032908becc423580708aec7f804eb
tmraaex <tmr.aaex@gmail.com>
2025-05-25 02:28:38 +0200

    added error handling

--- api/projects.js
+++ api/projects.js
@@ -1,14 +1,13 @@
 import client from "../lib/db";
 
 export default async function handler(req, res) {
-  const db_client = await client.connect();
-  const db = db_client.db("portfolio");
-  const collection = db.collection("Projects");
-
-
-  const projects = await collection.find({}).toArray();
-  
-  
-
-  res.status(200).json(projects);
+  try {
+    const db_client = await client.connect();
+    const db = db_client.db("portfolio");
+    const collection = db.collection("Projects");
+    const projects = await collection.find({}).toArray();
+    res.status(200).json(projects);
+  } catch (err) {
+    res.status(500).json({ message: "Oväntat fel" });
+  }
 }
